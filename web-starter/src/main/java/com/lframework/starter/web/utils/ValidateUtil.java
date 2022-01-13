51f731e4c18ed67e1a6ffaa1d65629511822b15a
zmj <lframework@163.com>
2022-01-13 14:27:48 +0800

    增加缓存、通用验证工具类

---
+++ web-starter/src/main/java/com/lframework/starter/web/utils/ValidateUtil.java
@@ -0,0 +1,40 @@
+package com.lframework.starter.web.utils;
+
+import com.lframework.common.utils.ArrayUtil;
+import com.lframework.common.utils.CollectionUtil;
+import com.lframework.common.utils.StringUtil;
+
+import java.util.Enumeration;
+import java.util.Map;
+
+public class ValidateUtil {
+
+    public static boolean isEmpty(Object val) {
+
+        if (val == null) {
+            return true;
+        }
+
+        if (val instanceof CharSequence) {
+            return StringUtil.isEmpty((CharSequence) val);
+        }
+
+        if (val instanceof Iterable) {
+            return CollectionUtil.isEmpty((Iterable<?>) val);
+        }
+
+        if (val instanceof Map) {
+            return CollectionUtil.isEmpty((Map<?, ?>) val);
+        }
+
+        if (val instanceof Enumeration) {
+            return CollectionUtil.isEmpty((Enumeration<?>) val);
+        }
+
+        if (val instanceof Object[]) {
+            return ArrayUtil.isEmpty((Object[]) val);
+        }
+
+        return false;
+    }
+}
