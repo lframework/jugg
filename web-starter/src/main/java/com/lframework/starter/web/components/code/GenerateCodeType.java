60a14f468c08210ea9bd736e0b3f8d757e35bcf8
lframework <lframework@163.com>
2022-03-02 19:35:08 +0800

    细化单号生成器，增加配置项

--- web-starter/src/main/java/com/lframework/starter/web/components/code/GenerateCodeType.java
+++ web-starter/src/main/java/com/lframework/starter/web/components/code/GenerateCodeType.java
@@ -13,4 +13,14 @@ public interface GenerateCodeType extends Serializable {
      * 默认的单号生成类型
      */
     GenerateCodeType DEFAULT = new DefaultGenerateCodeType();
+
+    /**
+     * 流水号类型的单号生成类型
+     */
+    GenerateCodeType FLOW = new DefaultFlowGenerateCodeType();
+
+    /**
+     * 雪花算法的单号生成类型
+     */
+    GenerateCodeType SNOW_FLAKE = new DefaultSnowFlakeGenerateCodeType();
 }
