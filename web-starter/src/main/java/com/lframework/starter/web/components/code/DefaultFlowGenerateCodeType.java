97c498841791b9458f0c9f340b9ce1e4564d0a9e
lframework <lframework@163.com>
2022-03-02 19:35:08 +0800

    细化单号生成器，增加配置项

---
+++ web-starter/src/main/java/com/lframework/starter/web/components/code/DefaultFlowGenerateCodeType.java
@@ -0,0 +1,5 @@
+package com.lframework.starter.web.components.code;
+
+public class DefaultFlowGenerateCodeType implements GenerateCodeType {
+    private static final long serialVersionUID = 1L;
+}
---
+++ web-starter/src/main/java/com/lframework/starter/web/components/generator/impl/DefaultFlowGenerator.java
@@ -0,0 +1,24 @@
+package com.lframework.starter.web.components.generator.impl;
+
+import com.lframework.starter.web.components.code.GenerateCodeType;
+import com.lframework.starter.web.components.generator.Generator;
+import org.springframework.stereotype.Component;
+
+/**
+ * 默认流水号生成器
+ * 流水号位数是10位，即：yyyyMMdd0000000001格式
+ */
+@Component
+public class DefaultFlowGenerator extends AbstractFlowGenerator implements Generator {
+
+    @Override
+    public GenerateCodeType getType() {
+
+        return GenerateCodeType.FLOW;
+    }
+
+    @Override
+    protected int getCodeLength() {
+        return 10;
+    }
+}
