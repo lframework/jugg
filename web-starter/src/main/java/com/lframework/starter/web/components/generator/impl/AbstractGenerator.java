e7a376d6860e95d040da062749df0d6fe84eeb4d
lframework <lframework@163.com>
2022-03-03 10:42:05 +0800

    单号生成器的内置类型允许自定义

--- web-starter/src/main/java/com/lframework/starter/web/components/generator/impl/AbstractGenerator.java
+++ web-starter/src/main/java/com/lframework/starter/web/components/generator/impl/AbstractGenerator.java
@@ -1,25 +1,11 @@
 package com.lframework.starter.web.components.generator.impl;
 
-import com.lframework.common.utils.IdWorker;
 import com.lframework.starter.web.components.generator.Generator;
-import com.lframework.starter.web.utils.ApplicationUtil;
 
-/**
- * 单号生成器
- *
- * @author zmj
- */
 public abstract class AbstractGenerator implements Generator {
 
     @Override
-    public String generate() {
-
-        IdWorker idWorker = ApplicationUtil.getBean(IdWorker.class);
-        return getPreffix() + idWorker.nextIdStr();
-    }
-
-    protected String getPreffix() {
-
-        return "";
+    public boolean isSpecial() {
+        return false;
     }
 }
