aa776e1bcd019fa4c44a67803807a32af5cdcf48
lframework <lframework@163.com>
2022-03-20 23:00:44 +0800

    update

--- web-starter/src/main/java/com/lframework/starter/web/config/UploadConfiguration.java
+++ web-starter/src/main/java/com/lframework/starter/web/config/UploadConfiguration.java
@@ -2,13 +2,9 @@ package com.lframework.starter.web.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 
 @Configuration
