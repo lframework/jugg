2657e5a55353c43e7a8cc8de1e616df0c2f69940
lframework <lframework@163.com>
2022-03-20 11:35:55 +0800

    增加打印

---
+++ web-starter/src/main/java/com/lframework/starter/web/bo/BasePrintDataBo.java
@@ -0,0 +1,16 @@
+package com.lframework.starter.web.bo;
+
+import com.lframework.starter.web.dto.BaseDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BasePrintDataBo<T extends BaseDto> extends BaseBo<T>{
+    public BasePrintDataBo() {
+    }
+
+    public BasePrintDataBo(T dto) {
+        super(dto);
+    }
+}
