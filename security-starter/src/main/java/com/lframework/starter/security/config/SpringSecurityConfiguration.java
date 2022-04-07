4f4b6841172dcddc501c9c4e720c8658e747be44
lframework <lframework@163.com>
2022-04-08 00:46:54 +0800

    修改配置

--- security-starter/src/main/java/com/lframework/starter/security/config/SpringSecurityConfiguration.java
+++ security-starter/src/main/java/com/lframework/starter/security/config/SpringSecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.lframework.starter.security.config;
 
 import com.lframework.starter.security.components.CheckPermissionHandler;
 import com.lframework.starter.security.components.CheckPermissionHandlerImpl;
+import com.lframework.starter.security.impl.message.TodoTaskServiceImpl;
+import com.lframework.starter.security.service.message.ITodoTaskService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +18,12 @@ public class SpringSecurityConfiguration {
   public CheckPermissionHandler checkPermissionHandler() {
 
     return new CheckPermissionHandlerImpl();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean(ITodoTaskService.class)
+  public ITodoTaskService getTodoTaskService() {
+
+    return new TodoTaskServiceImpl();
   }
 }
--- security-starter/src/main/java/com/lframework/starter/security/impl/message/TodoTaskServiceImpl.java
+++ security-starter/src/main/java/com/lframework/starter/security/impl/message/TodoTaskServiceImpl.java
@@ -3,11 +3,7 @@ package com.lframework.starter.security.impl.message;
 import com.lframework.starter.mybatis.resp.PageResult;
 import com.lframework.starter.security.bo.message.TodoTaskBo;
 import com.lframework.starter.security.service.message.ITodoTaskService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.stereotype.Service;
 
-@ConditionalOnMissingBean(ITodoTaskService.class)
-@Service
 public class TodoTaskServiceImpl implements ITodoTaskService {
 
   @Override
