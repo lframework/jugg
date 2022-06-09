4a6676f5c05ab60a216c65720450fafd409b5995
lframework <lframework@163.com>
2022-06-09 17:20:45 +0800

    解决lettuce连接失效问题

---
+++ web-starter/src/main/java/com/lframework/starter/web/config/LettuceConfiguration.java
@@ -0,0 +1,22 @@
+package com.lframework.starter.web.config;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LettuceConfiguration implements InitializingBean {
+
+  @Autowired
+  private RedisConnectionFactory redisConnectionFactory;
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    if (redisConnectionFactory instanceof LettuceConnectionFactory) {
+      LettuceConnectionFactory c = (LettuceConnectionFactory) redisConnectionFactory;
+      c.setValidateConnection(true);
+    }
+  }
+}
