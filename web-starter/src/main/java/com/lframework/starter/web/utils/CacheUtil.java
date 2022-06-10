7a141c115d4bb4713dc8b4e732950da237b9dd4a
lframework <lframework@163.com>
2022-06-10 21:31:37 +0800

    update

--- web-starter/src/main/java/com/lframework/starter/web/utils/CacheUtil.java
+++ web-starter/src/main/java/com/lframework/starter/web/utils/CacheUtil.java
@@ -1,22 +1,22 @@
 package com.lframework.starter.web.utils;
 
-import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.CacheManager;
 
 /**
  * 缓存工具类 基于SpringCache
  */
 public class CacheUtil {
 
-  private static final CachingConfigurer configurer = ApplicationUtil.getBean(CachingConfigurer.class);
+  private static CacheManager cacheManager = ApplicationUtil.getBean(CacheManager.class);
 
   public static <T> T get(String cacheName, Object key, Class<T> clazz) {
 
-    return configurer.cacheManager().getCache(cacheName).get(key, clazz);
+    return cacheManager.getCache(cacheName).get(key, clazz);
   }
 
   public static void put(String cacheName, Object key, Object value) {
 
-    configurer.cacheManager().getCache(cacheName).put(key, value);
+    cacheManager.getCache(cacheName).put(key, value);
   }
 
   public static void putIfNotEmpty(String cacheName, Object key, Object value) {
@@ -30,6 +30,6 @@ public class CacheUtil {
 
   public static void evict(String cacheName, Object key) {
 
-    configurer.cacheManager().getCache(cacheName).evict(key);
+    cacheManager.getCache(cacheName).evict(key);
   }
 }
